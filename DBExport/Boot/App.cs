2a91a8b30d22e4b2e16cb45189efa4dd8cbf772a
unknown <ialbantov@ws03.lizdevntd.local>
2015-08-13 19:21:22 +0300

    no message

---
+++ DBExport/Boot/App.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace DBExport.Boot
+{
+	class App : Application
+	{
+		protected override void OnStartup(StartupEventArgs e)
+		{
+			base.OnStartup(e);
+		}
+	}
+}
