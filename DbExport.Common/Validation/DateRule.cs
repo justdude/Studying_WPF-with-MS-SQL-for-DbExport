71d751a9ff6e54a57ca8803e18c82d9ba96db489
unknown <ialbantov@ws03.lizdevntd.local>
2015-08-14 18:00:38 +0300

    no message

---
+++ DbExport.Common/Validation/DateRule.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace CRM.Common.Validation
+{
+	public class DateRule : ValidationRule
+	{
+		private static DateTime CurrentDate = new DateTime(1985, 1, 1);
+
+		public override ValidationResult Validate(object value, System.Globalization.CultureInfo cultureInfo)
+		{
+			string errorContent = null;
+
+			if (value == null)
+			{
+				errorContent = "Empty datetime";
+				return new ValidationResult(false, errorContent);
+			}
+				
+
+			DateTime orderDate = (DateTime)value;
+
+			if (orderDate >= DateTime.Now)
+			{
+				errorContent = "Please, enter date before Now()";
+				return new ValidationResult(false, errorContent);
+			}
+
+			bool res = orderDate < CurrentDate;
+			errorContent = "No less than 01/01/1985";
+
+			if (!res)
+			{
+				errorContent = null;
+			}
+
+			return new ValidationResult(!res, errorContent);
+		}
+
+		public static bool IsValid(object value, out string errorContent)
+		{
+			errorContent = null;
+
+			if (value == null)
+			{
+				errorContent = "Empty datetime";
+				return false;
+			}
+
+
+			DateTime orderDate = (DateTime)value;
+
+			if (orderDate >= DateTime.Now)
+			{
+				errorContent = "Please, enter date before Now()";
+				return false;
+			}
+
+			bool res = orderDate < CurrentDate;
+			errorContent = "No less than 01/01/1985";
+
+			if (!res)
+			{
+				errorContent = null;
+			}
+
+			return !res;
+		}
+	}
+}
---
+++ DbExport.Common/Validation/NameRule.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace CRM.Common.Validation
+{
+	public class NameRule : ValidationRule
+	{
+		public override ValidationResult Validate(object value, System.Globalization.CultureInfo cultureInfo)
+		{
+			string str = value as string;
+
+			return new ValidationResult(str.Length > 10, "Must be more than 10 symbols");
+		}
+	}
+}
